c5780f2fc5707292d7cfd5656083df0804682563
andy <andyblacknred@gmail.com>
2025-11-05 13:49:37 +0200

    base dashboard layout

---
+++ src/components/Controls.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Controls: React.FC = () => {
+    return (
+        <div className="controls">
+            {/* buttons will be here */}
+        </div>
+    );
+};
+
+export default Controls;
---
+++ src/components/Grid.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Grid: React.FC = () => {
+    return (
+        <div className="dashboard-grid">
+            {/* cells will be here */}
+        </div>
+    );
+};
+
+export default Grid;
